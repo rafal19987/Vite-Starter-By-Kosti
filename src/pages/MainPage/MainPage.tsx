ad0467b4e56a02abf74262c63afc9f3d97bab733
rafal19987 <rafal987.1@o2.pl>
2023-02-12 10:18:01 +0100

    Updated dependencies

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MainPage = () => {
-    return <div className='mainPageContainer'></div>;
+	return <div className="mainPageContainer"></div>;
 };
 
 export default MainPage;
